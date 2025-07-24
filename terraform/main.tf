b9d58b4a2db7ed1ecfa9b32139283d1068fd89b9
ElMed05 <elmehdi.bejja@gmx.de>
2025-07-24 20:36:39 +0200

    11

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,10 +8,8 @@ terraform {
 }
 
 provider "kubernetes" {
-  host                   = yamldecode(file("${path.module}/.kubeconfig"))["clusters"][0]["cluster"]["server"]
-  client_certificate     = base64decode(yamldecode(file("${path.module}/.kubeconfig"))["users"][0]["user"]["client-certificate-data"])
-  client_key             = base64decode(yamldecode(file("${path.module}/.kubeconfig"))["users"][0]["user"]["client-key-data"])
-  cluster_ca_certificate = base64decode(yamldecode(file("${path.module}/.kubeconfig"))["clusters"][0]["cluster"]["certificate-authority-data"])
+  config_path = "${path.module}/.kubeconfig"
 }
+
 
 
