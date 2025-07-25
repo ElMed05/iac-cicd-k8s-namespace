6982c60c7923b2ed43ed6b870cd1995e097dff3d
ElMed05 <elmehdi.bejja@gmx.de>
2025-07-25 10:38:12 +0200

    test0

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.8.2"
+
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -6,6 +8,7 @@ terraform {
     }
   }
 }
+
 
 provider "kubernetes" {
   config_path = "${path.module}/.kubeconfig"
