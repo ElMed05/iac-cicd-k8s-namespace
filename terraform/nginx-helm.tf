461d3a9d16effb73363ca435ae4ca2e151deb272
ElMed05 <elmehdi.bejja@gmx.de>
2025-07-27 18:44:31 +0200

    fix_versionbitnami

--- terraform/nginx-helm.tf
+++ terraform/nginx-helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginx" {
   name       = var.nginx_release_name
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx"
-  version    = "17.4.0"
+  version    = "19.0.3"
   namespace  = var.namespace
 
   values = [
