58d528dfda1f63f10e2c6e03bb9e424badfe054f
ElMed05 <elmehdi.bejja@gmx.de>
2025-07-27 17:23:11 +0200

    code fix

--- terraform/nginx-helm.tf
+++ terraform/nginx-helm.tf
@@ -1,11 +1,12 @@
 resource "helm_release" "nginx" {
-  name       = "nginx"
-  namespace  = var.namespace
+  name       = var.nginx_release_name
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx"
-  version    = "15.5.3"
+  namespace  = var.namespace
 
-  values = [file("${path.module}/helm/nginx-values.yaml")]
+  values = [
+    file("${path.module}/${var.nginx_values_file}")
+  ]
 
   depends_on = [kubernetes_namespace.this]
 }
