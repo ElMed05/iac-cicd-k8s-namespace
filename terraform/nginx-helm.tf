a1bb5627ed84c0baed1697f5a5a90c2360224bd7
ElMed05 <elmehdi.bejja@gmx.de>
2025-07-27 20:43:06 +0200

    add_lokal_char

--- terraform/nginx-helm.tf
+++ terraform/nginx-helm.tf
@@ -1,8 +1,8 @@
 resource "helm_release" "nginx" {
   name       = var.nginx_release_name
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx"
-  version    = "21.0.8" # oder andere laut `helm search repo bitnami/nginx --versions`
+  #repository = "https://charts.bitnami.com/bitnami"
+  chart      = "${path.module}/helm/charts/nginx"
+  #version    = "21.0.8" # oder andere laut `helm search repo bitnami/nginx --versions`
   namespace  = var.namespace
 
   values = [
