a50940d96d7534af5e6cf3e2c2290c2069204ace
ElMed05 <elmehdi.bejja@gmx.de>
2025-07-27 17:49:15 +0200

    ficx_version

--- terraform/nginx-helm.tf
+++ terraform/nginx-helm.tf
@@ -2,6 +2,7 @@ resource "helm_release" "nginx" {
   name       = var.nginx_release_name
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx"
+  version    = "17.3.4"
   namespace  = var.namespace
 
   values = [
